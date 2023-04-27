a64f6da819d0b3679ddd53edd4e140f21ae84aad
Feisal Migo Keli <kellyfeisal2000@gmail.com>
2023-04-27 23:45:44 +0300

    actors update

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,23 @@
 import React from "react";
 import { actors } from "../data";
+import { Link } from "react-router-dom";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const act = actors;
+  return (
+    <div>
+      {act.map((acting) => {
+        return (
+          <ul key={acting.id}>
+            <Link to="/actors">
+              {" "}
+              <li>{acting}</li>
+            </Link>
+          </ul>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import {
+  Route,
+  Switch,
+  CreateBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
 
+const router = CreateBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/movies", element: <Movies /> },
+  { path: "/directors", element: <Directors /> },
+  { path: "/actors", element: <Actors /> },
+]);
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <RouterProvider route={router} />
+    </div>
+  );
 }
 
 export default App;
